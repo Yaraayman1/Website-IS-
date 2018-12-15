abebf948d2891beee20780aafbbc7be22f037539
yousef-nada <36379381+yousef-nada@users.noreply.github.com>
2018-12-15 15:37:00 +0200

    Update signup.inc.php

--- signup.inc.php
+++ signup.inc.php
@@ -4,18 +4,19 @@ if (isset($_POST['submit'] ) ) {
 	
 	include_once ' dbh.inc.php' ;
 	
-	$first = mysqli_real_escape_string($conn, $_POST[ ' first ' ]; 
-	$last = mysqli_real_escape_string($conn, $_POST[ ' last ' ]; 
-	$email = mysqli_real_escape_string($conn, $_POST[ ' email ' ]; 
-	$uid = mysqli_real_escape_string($conn, $_POST[ 'uid' ]; 
-	$pwd = mysqli_real_escape_string($conn, $_POST[ ' pwd ' ]; 
+	$first = mysqli_real_escape_string($conn, $_POST[ ' first ' ]); 
+	$last = mysqli_real_escape_string($conn, $_POST[ ' last ' ]); 
+	$email = mysqli_real_escape_string($conn, $_POST[ ' email ' ]); 
+	$uid = mysqli_real_escape_string($conn, $_POST[ 'uid' ]); 
+	$pwd = mysqli_real_escape_string($conn, $_POST[ ' pwd ' ]); 
 	
 	// Error handlers
 	// check for empty fields
-	if (empty($first) || empty  ($last) || empty ($email) || (empty($uid) || empty($pwd){
+	if ( empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd) ){
 		header( "Location: ../signup.php?signup=empty");
 		exit ( );
-	}  else {
+	}  
+	else {
 		// check if input characters are valid 
 		if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last)) {
 		header( "Location: ../signup.php?signup=invalid");
