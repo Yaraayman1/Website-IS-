a46cc7904abfe4440d47a3053e9e5e53e774c744
Yaraayman1 <36366173+Yaraayman1@users.noreply.github.com>
2018-12-15 12:32:15 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page Title</title>
+	<link rel="styl" type = "text/css" href="styl.css">
+	
+</head>
+<body>
+
+<header>
+	<nav> 
+		<div class = "main-wrapper"> 
+			<ul>
+				<li><a href ="index.php">Home</a></li>
+			</ul>
+			<div class = "nav-login">
+				<form>
+					<input type = "text" name = "uid" placeholder= "Username/e-mail">
+					<input type = "password" name = "pwd" placeholder= "password">
+					<button type = "submit" name = "submit ">Login</button>
+				</form>
+					<a href="signup.php">Sign up</a>
+			</div>
+		</div>
+</nav>
+</header>
